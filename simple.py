55372bd88c6ef73ed655ff127badbeed619a2fcb
Ajeya <ajeyamd3@gmail.com>
2023-05-30 15:19:24 +0530

    new

---
+++ simple.py
@@ -0,0 +1,8 @@
+from array import *
+
+arr1=array('i',[1,2,3,4,5])
+# print(arr1)
+
+arr1.insert(-1,11)
+print(arr1)
+
---
+++ traverse.py
@@ -0,0 +1,6 @@
+from array import *
+arr1=array('i',[1,2,3,4,5,6,7])
+def traverse(array):
+    for i in array:
+        print(i)
+traverse(arr1)
